29431d3ad11ca3e4a78d763672505aebc793b068
AutoCloneBot <acautoclone@bot.com>
2024-06-27 14:49:33 +0000

    Added my codes from AtCoder

---
+++ APG4b/APG4b_cg.cpp
@@ -0,0 +1,26 @@
+/**
+*    author:  rei10210
+*    created: 2024.06.27 15:09:13
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  vector<int> data(5);
+  for (int i = 0; i < 5; i++) {
+    cin >> data.at(i);
+  }
+
+  // dataの中で隣り合う等しい要素が存在するなら"YES"を出力し、そうでなければ"NO"を出力する
+  // ここにプログラムを追記
+  bool flag = false;
+  for (int i = 0; i < 4; i++) {
+    if (data.at(i) == data.at(i+1)) {
+      flag = true;
+      break;
+    }
+  }
+  if (flag) cout << "YES" << endl;
+  else cout << "NO" << endl;
+}
